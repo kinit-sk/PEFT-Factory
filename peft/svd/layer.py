8b4a24dd6e7d56581dccff46489aa86f41566094
Róbert Belanec <belanecrobert22@gmail.com>
2025-11-07 15:25:16 +0100

    Functioning custom PEFT interface, added BitFit, SVD method initialization

---
+++ peft/svd/layer.py
@@ -0,0 +1,15 @@
+
+import torch.nn as nn
+
+from peft.tuners.tuners_utils import BaseTunerLayer
+
+class SVDLayer(nn.Module, BaseTunerLayer):
+    adapter_layer_names = ("svd_layers",)
+
+    def __init__(self, base_layer: nn.Module, adapter_name: str):
+        super().__init__()
+        self.base_layer = base_layer
+        self.bitfit_layers = nn.ModuleDict({})
+        self.update_layer(self.base_layer, adapter_name)
+        self._active_adapter = adapter_name
+        self.merged_adapters = []
