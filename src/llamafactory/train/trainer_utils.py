9a77197bba486d0dad5c34f9af385ea59b35a9ff
Róbert Belanec <belanecrobert22@gmail.com>
2025-11-17 12:10:25 +0100

    trainer_utils

--- src/llamafactory/train/trainer_utils.py
+++ src/llamafactory/train/trainer_utils.py
@@ -1,3 +1,16 @@
+# Copyright 2025 the PEFTFactory team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 # Copyright 2025 HuggingFace Inc. and the LlamaFactory team.
 #
 # This code is inspired by the original GaLore's implementation: https://github.com/jiaweizzhao/GaLore
@@ -92,7 +105,7 @@ def create_modelcard_and_push(
     kwargs = {
         "tasks": {"name": "Causal Language Modeling", "type": "text-generation"},
         "finetuned_from": model_args.model_name_or_path,
-        "tags": ["llama-factory", finetuning_args.finetuning_type],
+        "tags": ["peft-factory", finetuning_args.finetuning_type],
     }
     if data_args.dataset is not None:
         kwargs["dataset"] = data_args.dataset
